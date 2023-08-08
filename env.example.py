52cd50d3d1282990eea69028d42a84d0d0f213f9
Denis Kostiv <deniskostiv@gmail.com>
2023-08-08 14:58:15 +0300

    README

---
+++ env.example.py
@@ -0,0 +1,13 @@
+# Django
+DJANGO_KEY = 'some key'
+
+# Database
+DB_NAME = 'mydatabase'
+DB_USER = 'username'
+DB_PASSWORD = 'password'
+DB_HOST = 'localhost'
+DB_PORT = '3306'
+
+# Telegram
+ADMIN_TOKEN = 'some token'
+MY_CHAT = 'some id'
